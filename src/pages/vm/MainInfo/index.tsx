7fb6da58834b44e77bc96459e55514d2eccd194c
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2026-01-30 14:44:00 +0300

    changed main info layout

--- src/pages/vm/MainInfo/index.tsx
+++ src/pages/vm/MainInfo/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
-import {Card, Form, Input, InputNumber, Typography, Flex, Select} from 'antd';
+import {
+  Card,
+  Form,
+  Input,
+  InputNumber,
+  Typography,
+  Flex,
+  Select,
+  Row,
+  Col,
+} from 'antd';
 
 const {Title, Text} = Typography;
 
@@ -17,93 +27,104 @@ const MainInfo: React.FC = () => {
         </Flex>
       }
     >
-      <Form.Item
-        label="Имя"
-        name="name"
-        rules={[
-          {
-            required: true,
-            message: 'Пожалуйста, введите имя виртуальной машины',
-          },
-        ]}
-      >
-        <Input placeholder="Введите имя виртуальной машины" />
-      </Form.Item>
+      <Row gutter={16}>
+        <Col span={12}>
+          <Form.Item
+            label="Имя"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: 'Пожалуйста, введите имя виртуальной машины',
+              },
+            ]}
+          >
+            <Input placeholder="Введите имя виртуальной машины" />
+          </Form.Item>
+        </Col>
+        <Col span={12}>
+          <Form.Item label="Описание" name="description">
+            <Input.TextArea
+              placeholder="Введите описание виртуальной машины"
+              autoSize={{minRows: 2}}
+            />
+          </Form.Item>
+        </Col>
+      </Row>
 
-      <Form.Item label="Описание" name="description">
-        <Input.TextArea
-          placeholder="Введите описание виртуальной машины"
-          autoSize={{minRows: 2}}
-        />
-      </Form.Item>
-
-      <Form.Item
-        label="Количество CPU"
-        name="cpuCount"
-        rules={[
-          {
-            required: true,
-            message: 'Пожалуйста, введите количество CPU',
-          },
-          {
-            type: 'number',
-            min: 1,
-            max: 10,
-            message: 'Количество CPU должно быть от 1 до 10',
-          },
-        ]}
-      >
-        <InputNumber
-          min={1}
-          max={10}
-          placeholder="Введите количество CPU"
-          style={{width: '100%'}}
-        />
-      </Form.Item>
-
-      <Form.Item
-        label="Объём RAM"
-        name="ramSize"
-        rules={[
-          {
-            required: true,
-            message: 'Пожалуйста, введите объём RAM',
-          },
-          {
-            type: 'number',
-            min: 1,
-            max: 256,
-            message: 'Объём RAM должен быть от 1 до 256 ГБ',
-          },
-        ]}
-      >
-        <InputNumber
-          min={1}
-          max={256}
-          placeholder="Введите объём RAM"
-          suffix="ГБ"
-          style={{width: '100%'}}
-        />
-      </Form.Item>
-
-      <Form.Item
-        label="Chipset"
-        name="chipset"
-        rules={[
-          {
-            required: true,
-            message: 'Пожалуйста, выберите chipset',
-          },
-        ]}
-      >
-        <Select
-          placeholder="Выберите chipset"
-          options={[
-            {label: 'q35', value: 'q35'},
-            {label: 'i440', value: 'i440'},
-          ]}
-        />
-      </Form.Item>
+      <Row gutter={16}>
+        <Col span={8}>
+          <Form.Item
+            label="Количество CPU"
+            name="cpuCount"
+            rules={[
+              {
+                required: true,
+                message: 'Пожалуйста, введите количество CPU',
+              },
+              {
+                type: 'number',
+                min: 1,
+                max: 10,
+                message: 'Количество CPU должно быть от 1 до 10',
+              },
+            ]}
+          >
+            <InputNumber
+              min={1}
+              max={10}
+              placeholder="Введите количество CPU"
+              style={{width: '100%'}}
+            />
+          </Form.Item>
+        </Col>
+        <Col span={8}>
+          <Form.Item
+            label="Объём RAM"
+            name="ramSize"
+            rules={[
+              {
+                required: true,
+                message: 'Пожалуйста, введите объём RAM',
+              },
+              {
+                type: 'number',
+                min: 1,
+                max: 256,
+                message: 'Объём RAM должен быть от 1 до 256 ГБ',
+              },
+            ]}
+          >
+            <InputNumber
+              min={1}
+              max={256}
+              placeholder="Введите объём RAM"
+              suffix="ГБ"
+              style={{width: '100%'}}
+            />
+          </Form.Item>
+        </Col>
+        <Col span={8}>
+          <Form.Item
+            label="Chipset"
+            name="chipset"
+            rules={[
+              {
+                required: true,
+                message: 'Пожалуйста, выберите chipset',
+              },
+            ]}
+          >
+            <Select
+              placeholder="Выберите chipset"
+              options={[
+                {label: 'q35', value: 'q35'},
+                {label: 'i440', value: 'i440'},
+              ]}
+            />
+          </Form.Item>
+        </Col>
+      </Row>
     </Card>
   );
 };
