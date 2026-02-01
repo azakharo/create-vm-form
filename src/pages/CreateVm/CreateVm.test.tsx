b6056a97603d55b981802d831040bbf8c4c02b81
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2026-02-01 12:10:17 +0300

    added test with Select

--- src/pages/CreateVm/CreateVm.test.tsx
+++ src/pages/CreateVm/CreateVm.test.tsx
@@ -3,6 +3,7 @@ import {CreateVmPage} from './index';
 import {render} from '../../utils/testing';
 import {describe, it, expect, vi} from 'vitest';
 import {createVm} from '../../api';
+import userEvent from '@testing-library/user-event';
 
 // Mock the createVm function
 vi.mock('../../api', () => ({
@@ -10,6 +11,10 @@ vi.mock('../../api', () => ({
 }));
 
 describe('CreateVmPage', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
   it('should submit form with correct values', async () => {
     render(<CreateVmPage />);
 
@@ -44,6 +49,61 @@ describe('CreateVmPage', () => {
       cpuCount: 1,
       ramSize: 4,
       chipset: 'q35',
+      hddDisks: [
+        {
+          id: 'disk-1',
+          name: 'disk-1',
+          enabled: true,
+          size: 128,
+          diskInterface: 'SCSI',
+        },
+      ],
+      bootOrder: ['disk-1'],
+    };
+
+    expect(createVm).toHaveBeenCalledWith(expectedValues);
+  });
+
+  it('should submit form with i440 chipset', async () => {
+    render(<CreateVmPage />);
+
+    // Fill the required "Name" field
+    const nameInput = screen.getByPlaceholderText(
+      /Введите имя виртуальной машины/i,
+    );
+    await waitFor(() => {
+      fireEvent.change(nameInput, {target: {value: 'Test VM'}});
+    });
+
+    // Change the Chipset field value to i440
+    const chipsetI440Option = await screen.findByText('i440');
+    await userEvent.selectOptions(screen.getByLabelText('Chipset'), [
+      chipsetI440Option,
+    ]);
+
+    // Create 1 HDD disk by clicking the "+" button
+    const addButton = screen.getByRole('button', {name: 'plus'});
+    await waitFor(() => {
+      fireEvent.click(addButton);
+    });
+
+    // Click the "Create" button
+    const createButton = screen.getByRole('button', {name: /Создать/i});
+    await waitFor(() => {
+      fireEvent.click(createButton);
+    });
+
+    // Verify createVm is called once with correct values
+    await waitFor(() => {
+      expect(createVm).toHaveBeenCalledTimes(1);
+    });
+
+    const expectedValues = {
+      name: 'Test VM',
+      description: '',
+      cpuCount: 1,
+      ramSize: 4,
+      chipset: 'i440',
       hddDisks: [
         {
           id: 'disk-1',
